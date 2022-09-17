8d5373231dac6cdbf255b59cc8acc1c44ffb6bae
sujin <soojin9877@naver.com>
2022-09-17 20:20:00 +0900

    Feat: Footer 컴포넌트 UI 완료

---
+++ src/components/Footer.tsx
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+import GithubIcon from '../assets/githubIcon.svg';
+
+const FooterEl = styled.footer`
+    background-color: #f2f2f2;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 15px 0;
+`;
+
+const Div = styled.div`
+    max-width: 1280px;
+    margin: 0 auto;
+`;
+
+const Ul = styled.ul`
+    padding: 54px 0 50px 0;
+    margin: 0 auto;
+    border-bottom: 1px solid #c4c4c4;
+`;
+
+const Li = styled.li`
+    float: left;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    ::before {
+        content: '';
+        display: inline-block;
+        width: 1px;
+        height: 14px;
+        margin: 0 8px;
+        background-color: black;
+        vertical-align: -2px;
+    }
+`;
+
+const GithubLink = styled.button`
+    float: right;
+    width: 32px;
+    height: 32px;
+    background-image: url(${GithubIcon});
+    background-repeat: no-repeat;
+    background-size: contain;
+`;
+
+const Address = styled.address`
+    padding: 30px 0 63px;
+    color: #767676;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+`;
+
+const Strong = styled.strong`
+    font-weight: 700;
+`;
+
+const Footer = () => {
+    return (
+        <FooterEl>
+            <Div>
+                <Ul>
+                    <Li>ㅇㅇ샵 소개</Li>
+                    <Li>이용약관</Li>
+                    <Li>개인정보처리방침</Li>
+                    <Li>전자금융거래약관</Li>
+                    <Li>청소년보호정책</Li>
+                    <Li>제휴문의</Li>
+                    <GithubLink />
+                </Ul>
+                <Address>
+                    <Strong>(주)ㅇㅇ SHOP</Strong>
+                    <br />
+                    제주특별자치도 제주시 한립읍 123
+                    <br /> 사업자 번호 : 123-4567-8910 | 통신판매업
+                    <br /> 대표 : 임수진
+                </Address>
+            </Div>
+        </FooterEl>
+    );
+};
+export default Footer;
