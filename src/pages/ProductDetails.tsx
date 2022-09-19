5fb3cb5b760ab30e9a6f316e518d39f5742e4810
sujin <soojin9877@naver.com>
2022-09-19 17:01:12 +0900

    Feat: ProductDetails 페이지 UI 완료

---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import ProductCard from 'components/ProductCard';
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+const Main = styled.main`
+    padding: 80px 0 60px;
+    width: 1300px;
+    margin: 0 auto;
+`;
+
+const Ul = styled.ul`
+    display: flex;
+    margin-top: 140px;
+    color: #767676;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 23px;
+    text-align: center;
+`;
+const Li = styled.li`
+    width: 320px;
+    padding: 19px 0 12px;
+    border-bottom: solid 6px #e0e0e0;
+`;
+
+const ProductDetails = () => {
+    return (
+        <>
+            <Header />
+            <Main>
+                <ProductCard />
+                <Ul>
+                    <Li>상세정보</Li>
+                    <Li>리뷰</Li>
+                    <Li>Q&A</Li>
+                    <Li>반품/교환정보</Li>
+                </Ul>
+            </Main>
+            <Footer />
+        </>
+    );
+};
+export default ProductDetails;
