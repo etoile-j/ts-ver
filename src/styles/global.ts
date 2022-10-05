9aa3f43135a909be89fef9c6a06425c55ce872b9
sujin <soojin9877@naver.com>
2022-10-05 15:10:19 +0900

    Feat: button cursor 속성 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,6 +20,9 @@ export const GlobalStyle = createGlobalStyle`
     border: none;
     outline: none;
   }
+  button{
+    cursor: pointer;
+  }
   h1, h2, h3, h4, h5, h6{
     font-family:'Maven Pro', sans-serif;
   }
