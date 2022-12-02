3a4492afaacc320c435c90bf45fe7cffa6e131dc
sujin <soojin9877@naver.com>
2022-12-02 17:01:58 +0900

    Modify: 캐러셀 이미지 수정

--- src/components/main/Carousel.tsx
+++ src/components/main/Carousel.tsx
@@ -1,7 +1,16 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Container, Temp, Temp2 } from './CarouselStyle';
+import {
+    Container,
+    Banner,
+    Banner2,
+    Banner3,
+    Wrap,
+    MainText,
+    SubText,
+    PointText,
+} from './CarouselStyle';
 
 const Carousel = () => {
     const settings = {
@@ -15,10 +24,20 @@ const Carousel = () => {
     return (
         <Container>
             <Slider {...settings}>
-                <Temp />
-                <Temp2 />
-                <Temp />
-                <Temp2 />
+                <Banner>
+                    {/* <Wrap>
+                        <MainText>OUR SHOP 이달의 이벤트</MainText>
+                    </Wrap> */}
+                </Banner>
+                <Banner2 onClick={() => (window.location.href = '/detail/298')}>
+                    <MainText>샐러드 무료 배송</MainText>
+                    <SubText>~ 재고 소진 시까지</SubText>
+                </Banner2>
+                <Banner3>
+                    <MainText>OO카드 간편 결제 시</MainText>
+                    <PointText>최대 7% 청구 할인</PointText>
+                    <SubText>개인 신용/체크 카드 한정 1인 1일 1회</SubText>
+                </Banner3>
             </Slider>
         </Container>
     );
--- src/components/main/CarouselStyle.ts
+++ src/components/main/CarouselStyle.ts
@@ -1,16 +1,67 @@
 import styled from 'styled-components';
+import banner from '../../assets/img-banner.png';
 
 const Container = styled.div`
     margin: 0 auto;
     padding-bottom: 80px;
 `;
 
-const Temp = styled.div`
-    background-color: lightblue;
+const Banner = styled.div`
+    padding-top: 150px;
+    background-color: #6997f7;
     height: 430px;
 `;
-const Temp2 = styled(Temp)`
-    background-color: lightgray;
+const Banner2 = styled(Banner)`
+    padding-left: 75px;
+    background-color: #d7d0ca;
+    background-image: url(${banner});
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: right;
+    cursor: pointer;
+`;
+const Banner3 = styled(Banner2)`
+    padding-top: 120px;
+    border-right: 60px solid #000000;
+    background-color: #6997f7;
+    background-image: none;
+    cursor: default;
 `;
 
-export { Container, Temp, Temp2 };
+const Wrap = styled.div`
+    width: 760px;
+    height: 430px;
+    margin: 0 auto;
+    text-align: center;
+    border: solid 1px pink;
+`;
+
+const MainText = styled.p`
+    color: #ffffff;
+    font-size: 50px;
+    font-weight: 600;
+`;
+
+const SubText = styled.p`
+    padding-top: 25px;
+    color: #ffffff;
+    font-size: 20px;
+    font-weight: 600;
+`;
+
+const PointText = styled.p`
+    padding: 7px 0 20px;
+    font-size: 55px;
+    font-weight: 700;
+`;
+
+export {
+    Container,
+    Banner,
+    Banner2,
+    Banner3,
+    Wrap,
+    MainText,
+    SubText,
+    PointText,
+};
