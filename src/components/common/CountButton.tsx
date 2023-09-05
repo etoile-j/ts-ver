5c340c760963dbfb0d20136573dff4b7d8a011dc
sujin <soojin9877@naver.com>
2023-09-05 14:57:27 +0900

    Refactor: 상품 수량 조절 버튼 코드 개선

--- src/components/common/CountButton.tsx
+++ src/components/common/CountButton.tsx
@@ -1,63 +1,44 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import { useState } from 'react';
 import { Count, CountBtn, CountBtnplus } from './CountButtonStyle';
 import Modal from 'components/modal/Modal';
 import ModalContainer from 'components/modal/ModalContainer';
 
-interface IProductDetailProps {
-    stocks?: number | undefined;
-    count?: number | undefined;
-    setCount?: Dispatch<SetStateAction<number>> | undefined;
+interface IProductDetail {
+    stocks: number | undefined;
+    count: number;
+    setCount: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const CountButton = (props: IProductDetailProps) => {
+const CountButton = (props: IProductDetail) => {
     const { stocks, count, setCount } = props;
-    const [disabledP, setDisabledP] = useState(false);
-    const [disabledM, setDisabledM] = useState(false);
     const [modal, setModal] = useState(false);
 
     const handleModal = () => {
         setModal(!modal);
     };
 
-    const plusBtn = () => {
-        if (disabledM === true) {
-            setDisabledM(false);
-        }
+    const handlePlusBtn = () => {
         if (count === stocks) {
-            handleModal();
-            setDisabledP(true);
-        } else {
-            setCount!(count! + 1);
+            return handleModal();
         }
+        if (count < stocks!) setCount(count + 1);
     };
 
-    const minusBtn = () => {
-        if (disabledP === true) {
-            setDisabledP(false);
-            setCount!(count! - 1);
-        } else if (count === 1) {
-            setDisabledM((isState: boolean) => !isState);
-        } else if (count! > 1) {
-            setCount!(count! - 1);
+    const handleMinusBtn = () => {
+        if (count === 1) {
+            return;
         }
+        setCount(count - 1);
     };
 
     return (
         <>
             <div>
-                <CountBtn
-                    onClick={minusBtn}
-                    disabled={disabledM}
-                    aria-label="수량 빼기"
-                >
+                <CountBtn onClick={handleMinusBtn} aria-label="수량 빼기">
                     -
                 </CountBtn>
                 <Count aria-label={`현재 수량 ${count}`}>{count}</Count>
-                <CountBtnplus
-                    onClick={plusBtn}
-                    disabled={disabledP}
-                    aria-label="수량 더하기"
-                >
+                <CountBtnplus onClick={handlePlusBtn} aria-label="수량 더하기">
                     +
                 </CountBtnplus>
             </div>
