c0b19e29f1a3943ae8a832daa6318e1a1c2cfab6
sujin <soojin9877@naver.com>
2024-02-08 17:31:34 +0900

    Add: 폼 메시지 상수 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,6 +9,12 @@ export const ORDER_KIND = {
     DIRECT_ORDER: 'direct_order',
 } as const;
 
+export const FORM_MSG = {
+    REQUIRED: '필수 정보입니다.',
+    ONLY_NUMBER: '숫자만 입력 가능합니다.',
+    INSUFFICIENT_LENGTH: '모두 입력해 주세요.',
+} as const;
+
 export const REGEX = {
     ONLY_NUMBER: /^[0-9]+$/,
     ONLY_LETTER: /^[ㄱ-ㅎ가-힣a-zA-Z]+$/,
