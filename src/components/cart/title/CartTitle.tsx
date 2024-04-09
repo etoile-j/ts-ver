665ebcfb0b915c98dac35b6b861682be6cf80ce8
Sujin <102905624+etoile-j@users.noreply.github.com>
2024-04-09 13:34:04 +0900

    Feat: 장바구니 전체 삭제 기능 (#27)
    * Feat: AllDeleteBtn 생성 및 적용
    
    * Feat: deleteAllCartItems API 생성 및 적용

--- src/components/cart/title/CartTitle.tsx
+++ src/components/cart/title/CartTitle.tsx
@@ -1,6 +1,9 @@
+import { useMutation } from 'react-query';
+import queryClient from 'queries/queryClient';
+import { deleteAllCartItems } from 'apis/cart';
 import { IProduct, ICheckedItems } from 'GlobalType';
 import { filterAllItems } from 'utils';
-import { TitleLi, Content } from './CartTitleStyle';
+import { TitleLi, Content, ContentWrap, AllDeleteBtn } from './CartTitleStyle';
 
 interface ICartTitleProps {
     cartProductDetails: IProduct[];
@@ -20,6 +23,15 @@ const CartTitle = (cartTitleProps: ICartTitleProps) => {
         }
     };
 
+    const { mutate: mutateDeleteAll } = useMutation(() => deleteAllCartItems(), {
+        onSuccess: () => queryClient.invalidateQueries(['cartData']),
+        onError: (err) => console.error(err),
+    });
+
+    const handleDeleteAll = () => {
+        mutateDeleteAll();
+    };
+
     return (
         <TitleLi>
             <Content width="90px">
@@ -34,7 +46,15 @@ const CartTitle = (cartTitleProps: ICartTitleProps) => {
                     }
                 />
             </Content>
-            <Content width="611px">상품정보</Content>
+            <Content width="611px">
+                <ContentWrap>
+                    <span style={{ display: 'flex', alignItems: 'center', width: '73px' }}>
+                        <AllDeleteBtn onClick={handleDeleteAll}>모두 비우기</AllDeleteBtn>
+                    </span>
+                    <span style={{ textAlign: 'center' }}>상품정보</span>
+                    <span></span>
+                </ContentWrap>
+            </Content>
             <Content width="248px">수량</Content>
             <Content width="329px">상품금액</Content>
         </TitleLi>
--- src/components/cart/title/CartTitleStyle.ts
+++ src/components/cart/title/CartTitleStyle.ts
@@ -4,15 +4,33 @@ import { tabeleContent } from 'styles/mixins';
 const TitleLi = styled.li`
     display: flex;
     background: var(--light-gray);
-    padding: 19px 0 18px;
+    padding: 10px 0;
     margin-bottom: 35px;
     border-radius: 10px;
     font-weight: 400;
     font-size: 18px;
-    line-height: 23px;
+    line-height: 39px;
 `;
 
 const Content = styled.span`
     ${tabeleContent}
 `;
-export { TitleLi, Content };
+
+const ContentWrap = styled.div`
+    display: flex;
+    justify-content: space-between;
+    text-align: center;
+`;
+
+const AllDeleteBtn = styled.button`
+    background-color: var(--base-gray);
+    width: 73px;
+    height: 30px;
+    border-radius: 5px;
+    color: var(--white);
+    font-weight: 550;
+    font-size: 12.5px;
+    line-height: 20px;
+`;
+
+export { TitleLi, Content, ContentWrap, AllDeleteBtn };
